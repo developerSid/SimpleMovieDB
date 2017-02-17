13e03d5b98dd17bf88e2db5f62a620c7f2941e24
Gary Myers <gary.myers@imod-mini-mac.imod.lan>
2017-02-16 22:20:51 -0600

    Initial project skeleton setup

---
+++ src/test/java/org/devict/SimpleMovieDbApplicationTests.java
@@ -0,0 +1,16 @@
+package org.devict;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleMovieDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
