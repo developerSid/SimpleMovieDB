ee84ea5f430567701d5b0139b239056f449b6a0d
developerSid <developerSid@gmail.com>
2017-02-16 22:40:04 -0600

    Repositories created

---
+++ src/main/groovy/org/devict/movie/db/repository/MovieRepository.java
@@ -0,0 +1,34 @@
+package org.devict.movie.db.repository;
+
+import org.devict.movie.db.entity.Movie;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by developerSid on 1/11/17.
+ *
+ * The {@link Movie} repository
+ */
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long>
+{
+   List<Movie> findByTitleContainingIgnoreCase(@Param("title") String title, Pageable pageable);
+
+   @EntityGraph(value = "graph.movies.complete", type = EntityGraphType.LOAD)
+   Optional<Movie> findMovieById(Long id);
+
+   @Query(value =
+        "select m "
+      + "from Movie m "
+      + "   join m.directors c "
+      + "where c.name = ?1")
+   List<Movie> findAllMoviesByDirector(String directorName);
+}
